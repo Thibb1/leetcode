6f7ff920f8bce58e7a2157d6c0fd8613806e4538
Thibb1 <thibault.brumaire@gmail.com>
2025-09-03 10:44:35 +0200

    + better longest substring

--- problems/longest-substring.cpp
+++ problems/longest-substring.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int bsf = 0;
-        int start = 0;
-        int end = 1;
-        for (; end <= s.size(); end++) {
-            string sub = s.substr(start, end - start);
-            int pos = sub.find(s[end]);
-            int len = sub.size();
-            bsf = max(bsf, len);
-            if (pos != string::npos) {
-                start += 1 + pos;
+        int left = 0;
+        int right = 0;
+        vector<int> chars(256);
+        fill(chars.begin(), chars.end(), -1);
+        for (; right < s.size(); right++) {
+            int pos = chars[s[right]];
+            chars[s[right]] = right;
+            if (pos != -1 && pos >= left) {
+                left = 1 + pos;
+            } else {
+                bsf = max(bsf, right - left + 1);
             }
         }
         return bsf;
